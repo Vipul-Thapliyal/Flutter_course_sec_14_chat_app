460bd39147c182b1b78d1d8f4ea0f5fd3574989a
VipulThapliyal <vipulthapliyal122@gmail.com>
2023-07-14 16:42:02 +0530

    Start With 267

---
+++ android/app/src/main/kotlin/com/example/chat_app_section_14/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.chat_app_section_14
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:chat_app_section_14/screens/auth.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'FlutterChat',
+      theme: ThemeData().copyWith(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromARGB(255, 63, 17, 177)
+        )
+      ),
+      home: const AuthScreen(),
+    );
+  }
+}
